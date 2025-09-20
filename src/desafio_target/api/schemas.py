08d17b6940d1a429a9c7e93f3781326547506c1a
guiioshua <guilhermeioshua@gmail.com>
2025-09-19 21:14:39 -0300

    style: renomeação das classes dos schemas de acionista para investidor

--- src/desafio_target/api/schemas.py
+++ src/desafio_target/api/schemas.py
@@ -2,10 +2,13 @@ from pydantic import BaseModel
 
 from typing import List
 
-class CotaPorAcionista(BaseModel):
+class CotaPorinvestidor(BaseModel):
     valor: float
-    quantidade_por_acionista: List[float]
+    quantidade_por_investidor: List[float]
 
 class RequisicaoTaxa(BaseModel):
     taxa: float
-    cotas_por_acionista: List[CotaPorAcionista]
+    cotas_por_investidor: List[CotaPorinvestidor]
+
+class RespostaTaxa(BaseModel):
+    taxas_por_investidor: List[float]
